80c43c12b4dd27ad18e86ee150c33753451ca5fe
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-06-03 17:54:19 +0800

    🐛 hashrouter for gh pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import FixedNavbar from "./Components/FixedNavbar";
 import Hero from "./Pages/Landing/Hero";
 import DotPattern from "./Components/Dot-pattern";
