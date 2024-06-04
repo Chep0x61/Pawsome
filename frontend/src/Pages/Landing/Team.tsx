b3857b9670db6ffbd0be6dbb33b9e84d0d1ce227
Thibault Thuillier <thibault.thuillier@epitech.eu>
2024-06-04 19:04:41 +0800

    🍱 pics of the team

--- frontend/src/Pages/Landing/Team.tsx
+++ frontend/src/Pages/Landing/Team.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import TeamCard from '../../Components/TeamCard';
+import Thibault from '../../Assets/Team/thibault.png'
+import Jeanne from '../../Assets/Team/jeanne.png'
+import Jozuha from '../../Assets/Team/jozuha.png'
+import Mathias from '../../Assets/Team/mathias.png'
+
 
 interface TeamMember {
     id: number;
@@ -11,11 +16,10 @@ interface TeamMember {
 
 const Team: React.FC = () => {
     const teamMembers: TeamMember[] = [
-        { id: 1, firstName: 'Jeanne', lastName: 'Boulet', position: 'Team Frontend', image: '/victor.guyot@epitech.eu.jpg' },
-        { id: 2, firstName: 'Jozuha', lastName: 'Bastiani', position: 'Team Hardware', image: '/maxence.amouroux@epitech.eu.jpg' },
-        { id: 3, firstName: 'Mathias', lastName: 'De Busschere', position: 'Team Hardware', image: '/alexandre.coyras@epitech.eu.jpg' },
-        { id: 4, firstName: 'Thibault', lastName: 'Thuillier', position: 'Team Frontend', image: '/thibault.thuillier@epitech.eu.jpg' },
-
+        { id: 1, firstName: 'Jeanne', lastName: 'Boulet', position: 'Team Frontend', image: Jeanne },
+        { id: 2, firstName: 'Jozuha', lastName: 'Bastiani', position: 'Team Hardware', image: Jozuha },
+        { id: 3, firstName: 'Mathias', lastName: 'De Busschere', position: 'Team Hardware', image: Mathias },
+        { id: 4, firstName: 'Thibault', lastName: 'Thuillier', position: 'Team Frontend', image: Thibault },
     ];
 
     return (
